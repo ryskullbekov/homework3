f6bc841d18f208f6fbdeeb3448df4cd0a793676f
ryskullbekov <bpench@bk.ru>
2021-08-24 18:31:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+////	// write your code here
+////        int num [] = {45,76,540,-20,65};
+////        System.out.println(num[1]);
+////        //Перезапись элемента
+////        num [2] = 456;
+////        //Двумерный массив
+//        System.out.println(num [2]);
+//        int num2 [] [] = new int [2] [3];
+//        num2 [0] [0] = 65;
+//        num2 [0] [1] = 32;
+//        num2 [0] [2] = -15;
+//        num2 [1] [0] = 788;
+//        num2 [1] [1] = 90;
+//        num2 [1] [2] = -312;
+//
+//        System.out.println(num2[0] [1]);
+//
+//
+
+//        int i = 1;
+//        while (i < 20){
+//            System.out.println("i = " + i); i++;
+//        }
+
+//        int num [][]=new int [1] [2];
+//        num [0] [0] = 76;
+//        num [0] [1] = 98;
+//        num [0] [2] = 250;
+//        num [1] [0] = 345;
+//        num [1] [1] = 12;
+//        num [1] [2] = 345;
+
+//
+        String name [] = {"Баястан","Руслан","Нюша"};
+        System.out.println(" Здравствуйте : " + name[0] + " , " + name[1] + " , " + name[2] );
+        switch (name[0]){
+            case "Баястан":
+                System.out.println("Доброе утро : Баястан");
+            case "Руслан":
+                System.out.println("Добрый день : Руслан");
+            case "Нюша":
+                System.out.println("Добрый вечер : Нюша");
+        }
+
+
+
+
+    }
+
+
+}
